14fd253f2278bc1307a2a3693a50cd7208a647f1
zhangligopher <zhangli344236745@163.com>
2021-12-08 18:12:46 +0800

    first commit

---
+++ zhihu/zhihu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+import datetime
+import requests
+header = {
+        'Host': 'www.zhihu.com',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept': '*/*',
+        'User-Agent': 'PostmanRuntime/7.17.1',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+}
+proxies = {
+    'https': '163.204.244.184:9999'
+}
+url = 'https://www.zhihu.com/api/v4/members/guodongxiaren?include=follower_count%2cvoteup_count'
+#resp = requests.get(url, headers=header, proxies=proxies)
+resp = requests.get(url, headers=header)
+data = resp.json()
+voteupCount = data['voteup_count']
+followerCount = data['follower_count']
+
+date = datetime.datetime.now().strftime('%Y-%m-%d')
+line = '%s,%s,%s' % (date, voteupCount, followerCount)
+print(line)
